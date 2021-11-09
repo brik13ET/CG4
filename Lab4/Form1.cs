44087e1c342a1d211862046af1a27a699a019443
brik13ET <aleev.ibragim0909@gmail.com>
2021-11-10 00:48:55 +0400

    Initial

--- Lab4/Form1.cs
+++ Lab4/Form1.cs
@@ -16,5 +16,31 @@ namespace Lab4
 		{
 			InitializeComponent();
 		}
+
+		private void toolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+
+
+			}
+		}
+
+		private void pictureBox1_Resize(object sender, EventArgs e)
+		{
+
+		}
+
+		static Color grayscale(Color c)
+		{
+			byte gray = (byte)(c.R * 0.2 + c.G * 0.4 + c.B * 0.1);
+			return Color.FromArgb(255, gray, gray, gray);
+		}
+
+		private void pictureBox2_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
